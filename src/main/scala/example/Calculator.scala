8b8e2096d42a2ecd6dd9eee20adc53e65ba99ae3
Fabian Gutierrez <fagossa@gmail.com>
2018-01-24 20:22:18 +0100

    improve documentation on Monoid

--- src/main/scala/example/Calculator.scala
+++ src/main/scala/example/Calculator.scala
@@ -4,7 +4,12 @@ object Calculator {
 
   import cats.Monoid
 
-  // TODO: implement the generic add on any monoid
+  /*
+   * TODO 03: implement the generic 'add' on any monoid
+   * Note:
+   * - accept a List of T
+   * - accept an implicit monoid of T
+   */
   def add[T](items: List[T])(implicit m: Monoid[T]): T = {
     import cats.syntax.semigroup._
     items.foldLeft(Monoid[T].empty)(_ |+| _)
