278d15489add713554e07970191b19df324a1b08
joriscode <2750485+joriscode@users.noreply.github.com>
2019-02-01 00:33:13 +0100

    Clean indentation

--- src/test/scala/example/ValidationSpec.scala
+++ src/test/scala/example/ValidationSpec.scala
@@ -85,7 +85,7 @@ class ValidationSpec extends WordSpec with MustMatchers {
 
       "handle multiple errors" in {
         // TODO 07: make this test pass
-       /* val params = Map.empty[String, String]
+        /* val params = Map.empty[String, String]
         validateUser(params) must be(List("name is not present", "age is not present").invalid[List[String]])*/
         fail("WIP")
       }
