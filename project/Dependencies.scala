708b518b491526b4cea0ad4f0c4c88f3f701742d
baddoub <badr.baddou@gmail.com>
2020-01-05 17:41:45 +0100

    Upgrade/uprgade scala sbt version (#5)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
   //lazy val scalaTest = "com.ironcorelabs" %% "cats-scalatest" % "2.3.0" % "test"
-  lazy val cats_core = "org.typelevel" %% "cats-core" % "1.0.0-RC1"
+  lazy val cats_core = "org.typelevel" %% "cats-core" % "2.1.0"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,1 @@
 addSbtPlugin("com.lucidchart" % "sbt-scalafmt" % "1.14")
-
-addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0")
